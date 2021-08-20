30c578cb11eb67f08194ece69fc88df4c1f26053
Brian Palay <bpplaysguitar@hotmail.com>
2021-08-20 11:40:12 -0500

    deploying

---
+++ server.js
@@ -0,0 +1,9 @@
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
